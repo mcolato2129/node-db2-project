0ab6df6536735b9cc29df00d724fcb180ed9293c
Marlon Colato <mcolato2104@gmail.com>
2024-01-27 16:12:59 -0500

    j

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,5 +1,19 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments()
+   
+    tbl.string('vin', 17).notNullable().unique()
+    
+    tbl.string('make', 129).notNullable()
+    
+    tbl.string('model', 129).notNullable()
+    
+    tbl.numeric('mileage').unsigned().notNullable()
+    
+    tbl.string('title', 129)
+    
+    tbl.string('transmission', 129)
+  })
 };
 
 exports.down = function (knex) {
